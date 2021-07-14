3d177c82493533919c86cd05b45de79e501cdb30
karimnoorani <59754833+karimnoorani@users.noreply.github.com>
2021-07-14 11:17:46 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import BookEntry from './components/BookEntry'
+
+function App(){
+    return(
+        <div>
+            <Header />
+            <BookEntry />
+        </div>
+    )
+}
+
+export default App
